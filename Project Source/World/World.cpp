96f5cfe23c171de5310361016d6bd706960687e7
rdmaranville <rdmaranville@alaska.edu>
2017-11-30 11:12:16 -0900

    adding world with sfml

--- Project Source/World/World.cpp
+++ Project Source/World/World.cpp
@@ -18,7 +18,87 @@ World::World(Board& board) : _board(board)
 
 World& World::build(Board& b)
 {
-    //Build the graphics etc for the world
+	//0 = White || 1 = Black
+	//Textures
+	Texture PawnTexture;
+	if (!PawnTexture.loadFromFile("Chess Pieces\\Pawn\\l0_ChessPieces6.png"))
+	{
+		std::cout << "Failure" << std::endl;
+	}
+	Texture RookTexture;
+	if (!RookTexture.loadFromFile("Chess Pieces\\Rook\\l0_ChessPieces3.png"))
+	{
+		std::cout << "Failure" << std::endl;
+	}
+	Texture BishopTexture;
+	if (!BishopTexture.loadFromFile("Chess Pieces\\Bishop\\l0_ChessPieces4.png"))
+	{
+		std::cout << "Failure" << std::endl;
+	}
+	Texture KnightTexture;
+	if (!KnightTexture.loadFromFile("Chess Pieces\\Knight\\l0_ChessPieces5.png"))
+	{
+		std::cout << "Failure" << std::endl;
+	}
+	Texture QueenTexture;
+	if (!QueenTexture.loadFromFile("Chess Pieces\\Queen\\l0_ChessPieces2.png"))
+	{
+		std::cout << "Failure" << std::endl;
+	}
+	Texture KingTexture;
+	if (!KingTexture.loadFromFile("Chess Pieces\\King\\l0_ChessPieces1.png"))
+	{
+		std::cout << "Failure" << std::endl;
+	}
+	vector <Texture> TileTexture(2);
+	if (!TileTexture[0].loadFromFile("Chess Pieces\\CheckerBoardWhite.png"))
+	{
+		std::cout << "Failure" << std::endl;
+	}
+	if (!TileTexture[1].loadFromFile("Chess Pieces\\CheckerBoardBlack.png"))
+	{
+		std::cout << "Failure" << std::endl;
+	}
+	//Sprite
+	//First Number is Color (0 = White || 1 = Black)
+	//Second Number is onscreen Number
+	vector <vector <Sprite>> PawnSprite(2, vector <Sprite>(8));
+	for (int loop = 0; loop < PawnSprite[0].size(); loop++)
+	{
+		PawnSprite[0][loop].setTexture(PawnTexture);
+		PawnSprite[1][loop].setTexture(PawnTexture);
+		PawnSprite[1][loop].setColor(Color(50, 50, 50));
+	}
+	vector <vector <Sprite>> RookSprite(2, vector <Sprite>(2));
+	for (int loop = 0; loop < RookSprite[0].size(); loop++)
+	{
+		RookSprite[0][loop].setTexture(RookTexture);
+		RookSprite[1][loop].setTexture(RookTexture);
+		RookSprite[1][loop].setColor(Color(50, 50, 50));
+	}
+	vector <vector <Sprite>> BishopSprite(2, vector <Sprite>(2));
+	for (int loop = 0; loop < BishopSprite[0].size(); loop++)
+	{
+		BishopSprite[0][loop].setTexture(BishopTexture);
+		BishopSprite[1][loop].setTexture(BishopTexture);
+		BishopSprite[1][loop].setColor(Color(50, 50, 50));
+	}
+	vector <vector <Sprite>> KnightSprite(2, vector <Sprite>(2));
+	for (int loop = 0; loop < KnightSprite[0].size(); loop++)
+	{
+		KnightSprite[0][loop].setTexture(KnightTexture);
+		KnightSprite[1][loop].setTexture(KnightTexture);
+		KnightSprite[1][loop].setColor(Color(50, 50, 50));
+	}
+	vector <Sprite> QueenSprite(2);
+	QueenSprite[0].setTexture(QueenTexture);
+	QueenSprite[1].setTexture(QueenTexture);
+	QueenSprite[1].setColor(Color(50, 50, 50));
+	vector <Sprite> KingSprite(2);
+	KingSprite[0].setTexture(KingTexture);
+	KingSprite[1].setTexture(KingTexture);
+	KingSprite[1].setColor(Color(50, 50, 50));
+	vector <Sprite> TileSprite(64);
     return *this;
 }
 
